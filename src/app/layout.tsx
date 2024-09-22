06783212ee8ca4a0a28c72ae24262fc776c4a5a3
Shadman Shakib <sm.shakib007@gmail.com>
2024-09-22 19:20:55 +0600

    ui: Sidebar and Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
 export const metadata = {
-  title: "Kinde Auth",
-  description: "Kinde with NextJS App Router",
+  title: "Linkaraby",
+  description: "Affiliate Marketing in Saudi, the Gulf and arab world",
 };
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
@@ -28,45 +28,7 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body>
-        <header>
-          <nav className="nav container">
-            <h1 className="text-display-3">Linkaraby</h1>
-            <div>
-              {!(await isAuthenticated()) ? (
-                <>
-                  <LoginLink className="btn btn-ghost sign-in-btn">
-                    Sign in
-                  </LoginLink>
-                  <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
-                </>
-              ) : (
-                <div className="profile-blob">
-                  {user?.picture ? (
-                    <img
-                      className="avatar"
-                      src={user?.picture}
-                      alt="user profile avatar"
-                      referrerPolicy="no-referrer"
-                    />
-                  ) : (
-                    <div className="avatar">
-                      {user?.given_name?.[0]}
-                      {user?.family_name?.[0]}
-                    </div>
-                  )}
-                  <div>
-                    <p className="text-heading-2">
-                      {user?.given_name} {user?.family_name}
-                    </p>
-
-                    <LogoutLink className="text-subtle">Log out</LogoutLink>
-                  </div>
-                </div>
-              )}
-            </div>
-          </nav>
-        </header>
+      <body dir={locale === "ar" ? "rtl" : "lrt"}>
         <main>
           <NextIntlClientProvider messages={messages}>
             {children}
