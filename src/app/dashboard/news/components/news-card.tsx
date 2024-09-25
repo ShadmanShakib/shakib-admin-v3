a3640be00ae46b90aeb5a11920acde1fd0074bfb
Shadman Shakib <sm.shakib007@gmail.com>
2024-09-25 08:37:42 +0600

    news: ui updated

--- src/app/dashboard/news/components/news-card.tsx
+++ src/app/dashboard/news/components/news-card.tsx
@@ -2,6 +2,7 @@ import { News } from "@prisma/client";
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
+import { Edit } from "lucide-react";
 
 type Props = {
   data: News;
@@ -11,7 +12,7 @@ export default function NewsCard({ data }: Props) {
   return (
     <Link href={`/dashboard/news/${data.id}`}>
       <Card className="hover:bg-gray-50 hover:shadow-xl">
-        <CardHeader>
+        <CardHeader className="relative">
           <CardTitle>{data.title}</CardTitle>
         </CardHeader>
         <CardContent>
