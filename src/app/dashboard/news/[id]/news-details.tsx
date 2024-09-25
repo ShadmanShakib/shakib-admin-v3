68b766f50092930413d062dade7d62e12c92c9c2
Shadman Shakib <sm.shakib007@gmail.com>
2024-09-25 17:56:59 +0600

    feature/news: news details page

--- src/app/dashboard/news/[id]/news-details.tsx
+++ src/app/dashboard/news/[id]/news-details.tsx
@@ -1,24 +1,46 @@
+"use client";
 import { News } from "@prisma/client";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { useTranslations } from "next-intl";
 import { format } from "date-fns";
+import { Button } from "@/components/ui/button";
+import { Edit, Trash } from "lucide-react";
 
 type Props = {
   data: News | null;
 };
 
 export default function NewsDetails({ data }: Props) {
+  const t = useTranslations();
   return (
-    <div className="max-w-screen-md mx-auto">
+    <div className="max-w-screen-md mx-auto py-3">
       <div className="">
-        <h1>{data?.title}</h1>
-        <p>{data?.content}</p>
-        <div className="">
-          <h1>User Email</h1>
-          <p>{data?.user_email}</p>
+        <div className="flex justify-between">
+          <h1 className="text-xl font-semibold pb-4">{data?.title}</h1>
+        </div>
+        <div className="py-3">
+          <p>{data?.content}</p>
         </div>
         <hr className="my-4" />
-        <p>{format(data?.created_at ?? new Date(), "MMMM do, yyyy")}</p>
+        <div className="flex gap-3">
+          <h1 className="">User Email:</h1>
+          <p>{data?.user_email}</p>
+        </div>
+        <div className="flex gap-3">
+          <h1>Published:</h1>
+          <p>{format(data?.created_at ?? new Date(), "MMMM do, yyyy")}</p>
+        </div>
+        <div className="mt-4 flex gap-4">
+          <Button className="flex gap-2" variant="secondary">
+            <Edit className="h-4 w-4" />
+            {t("Buttons.edit")}
+          </Button>
+          <Button className="flex gap-2" variant="destructive">
+            <Trash className="h-4 w-4" />
+            {t("Buttons.delete")}
+          </Button>
+        </div>
       </div>
     </div>
   );
