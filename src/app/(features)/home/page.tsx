765d2689f9fdc530a11972a5f383bb66ff53c14b
Shadman Shakib <sm.shakib007@gmail.com>
2024-10-12 20:45:35 +0600

    ui: added bar chart

--- src/app/(features)/home/page.tsx
+++ src/app/(features)/home/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Greeting from "./components/greeting";
+import { Revenue } from "./components/revenue";
 
 export default function Homepage() {
   return (
     <main className="px-5">
       <h1 className="my-5 text-2xl">Homepage</h1>
       <Greeting />
+      <Revenue />
     </main>
   );
 }
