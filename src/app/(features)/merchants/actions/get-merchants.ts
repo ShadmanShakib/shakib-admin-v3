21afa2eab7f75fb78fd85c892b90d2824103813f
Shadman Shakib <sm.shakib007@gmail.com>
2024-10-12 19:18:18 +0600

    merchant: api for next and previous page

--- src/app/(features)/merchants/actions/get-merchants.ts
+++ src/app/(features)/merchants/actions/get-merchants.ts
@@ -1,10 +1,11 @@
 "use server";
 import { db } from "@/lib/db";
-import { take } from "underscore";
 
-export default async function getMerchants() {
+export default async function getMerchants(page: number) {
+  const skip = (page - 1) * 10;
   const merchants = await db.merchants.findMany({
     take: 10,
+    skip,
   });
   //get total merchants
   const totalMerchants = await db.merchants.count();
