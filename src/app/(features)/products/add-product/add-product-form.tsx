0a2da8a84b2429fbdf4c59cd06638b9e5092edc0
Shadman Shakib <sm.shakib007@gmail.com>
2024-10-14 22:12:44 +0600

    ui: Add product

--- src/app/(features)/products/add-product/add-product-form.tsx
+++ src/app/(features)/products/add-product/add-product-form.tsx
@@ -6,6 +6,15 @@ import { useTranslations } from "next-intl";
 import { useForm } from "react-hook-form";
 import { Textarea } from "@/components/ui/textarea";
 import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   Form,
   FormField,
   FormLabel,
@@ -60,6 +69,35 @@ export default function AddProductForm({}: Props) {
                   </FormItem>
                 )}
               />
+
+              <h1>{t("Common.category")}</h1>
+              <div className="">
+                <FormField
+                  name="category"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="category">
+                        {t("Product_News.product_category")}
+                      </FormLabel>
+                      <FormControl>
+                        <Select {...field}>
+                          <SelectTrigger>
+                            <SelectValue />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup>
+                              <SelectItem value="1">Category 1</SelectItem>
+                              <SelectItem value="2">Category 2</SelectItem>
+                              <SelectItem value="3">Category 3</SelectItem>
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
             </form>
           </Form>
         </CardContent>
